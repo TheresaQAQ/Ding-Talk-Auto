7497ba2d7ae643497f3143207f960c5bc807a66a
TheresaQAQ <2081633657@qq.com>
2020-04-17 20:10:25 +0800

    Fixed a bug Optimize QQ group sign in

--- DingTalkAuto.py
+++ DingTalkAuto.py
@@ -1,4 +1,4 @@
-import pyautogui, os, time, threading
+import pyautogui, os, time,sys
 from datetime import datetime
 
 
@@ -57,17 +57,20 @@ def now_time():  # 获取系统时间，返回列表
 def get_second(x):
     return x['hour'] * 3600 + x['minute']*60 +x['second']
 
-def threadingQQ():
-    pass
-
 
 def main():
     startTime = StrT0List('8:30 9:20 10:10 11:00 14:30 15:20')
     finishTime = StrT0List('9:10 10:00 10:50 11:40 15:10 16:00')
     os.popen('C:\\Program Files (x86)\\DingDing\\DingtalkLauncher.exe')  # 打开钉钉
     print('打开钉钉主程序......')
-    time.sleep(1)
     while True:
+        while True:
+            if pyautogui.locateOnScreen('pic\\DingTalkGroupStatus2.png') != None:
+                break
+            else:
+                pyautogui.moveTo(pyautogui.locateOnScreen('pic\\DingTalkGroupStatus1.png'),duration=0.5)
+                pyautogui.click()
+                pyautogui.moveRel(100,100)
         try:
             if pyautogui.locateOnScreen('pic\\live.png') != None:
                 print('正在直播...')
@@ -80,12 +83,17 @@ def main():
             else:
                 for i in range(len(startTime) - 1):
                     if get_second(finishTime[i]) < get_second(now_time()) < get_second(startTime[i+1]):
-                        diff_time = get_second(startTime[i+1]) - get_second(now_time())
+                        diff_time = get_second(startTime[i + 1]) - get_second(now_time())
                         print('已下课!',diff_time,'S后上课')
-                        Sign_in_QQ()
-                        if diff_time > 30:
+                        if diff_time >= 600:
+                            time.sleep(diff_time - 600)
+                            Sign_in_QQ()
+                            time.sleep(600)
+                        elif 30 < diff_time < 600:
+                            Sign_in_QQ()
                             time.sleep(diff_time-30)
                         else:
+                            Sign_in_QQ()
                             time.sleep(diff_time)
                         break
                 for i in range(len(startTime)):
@@ -101,6 +109,8 @@ def main():
                         time.sleep(diff_time - 30)
                     else:
                         time.sleep(diff_time)
+                if get_second(now_time()) > get_second(finishTime[-1]):
+                    print('今天没课了，你看尼玛呢')
         except:
             print('error')
 
